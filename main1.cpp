71587c5a9d4cc3e085cba2c407406cedbbddc1c8
tanya <shanya.2018td@gmail.com>
2020-05-13 16:59:14 +0530

    display function added

--- main1.cpp
+++ main1.cpp
@@ -1,3 +1,20 @@
 #include<iostream>
 #inclde<time.h>
 using namespace std;
+
+char matrix[3][3]={'1','2','3','4','5','6','7','8','9'};
+char field;
+
+//Print the matrix
+void display()
+{
+	for(int i=0;i<3;i++)
+	{
+		for(int j=0;j<3;j++)
+		{
+			cout<<matrix[i][j]<<" ";
+		}
+		cout<<endl;
+	}  
+	
+}
