d9bba54ed3edf2b228f43b9dbe4ac339ecf68b3e
tanya <shanya.2018td@gmail.com>
2020-05-13 18:08:43 +0530

    inputBot function added

--- main1.cpp
+++ main1.cpp
@@ -25,11 +25,31 @@ void getInput()
 	cin>>field;
 	field--;
     if(matrix[a/3][a%3] != 'X' && matrix[a/3][a%3] != 'O' )
-    {matrix[a/3][a%3] = 'X'}
+    {
+	 matrix[a/3][a%3] = 'X';
+	 display();
+	}
     else
 	{
 	 cout<< "ILLEGAL MOVE"<<endl;
 	 cout<<"Now chance will be given to the bot"<<endl;
 	}
-	
+
 }
+void inputBot()
+{
+	cout<<"It's bot's chance"<<endl;
+	srand(time(0));
+	field=rand()%9;
+	if(matrix[a/3][a%3] != 'X' && matrix[a/3][a%3] != 'O' )
+    {
+	 matrix[a/3][a%3] = 'O';
+	 display();
+    }
+    else
+	{
+	 cout<< "ILLEGAL MOVE"<<endl;
+	 cout<<"Now chance will be given to the player2"<<endl;
+	}
+}
+
